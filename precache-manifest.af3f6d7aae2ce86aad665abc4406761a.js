1fff40dd6eeed32d6c0ee61ebb31d391f284cb55
viral sheladiya <sheladiyaviral2071998@gmail.com>
2022-07-19 10:20:45 +0530

    Updates

---
+++ precache-manifest.af3f6d7aae2ce86aad665abc4406761a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cec0ad55a8669ff3487d",
+    "url": "/My_app/static/css/main.03d9fcc0.chunk.css"
+  },
+  {
+    "revision": "cec0ad55a8669ff3487d",
+    "url": "/My_app/static/js/main.cec0ad55.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/My_app/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "08d5e9aaf11e1005ba80",
+    "url": "/My_app/static/js/2.08d5e9aa.chunk.js"
+  },
+  {
+    "revision": "0d3aba36d7fd9ef614a2",
+    "url": "/My_app/static/js/runtime~main.0d3aba36.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/My_app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "941037e6aa5a23a71496b432672dc08b",
+    "url": "/My_app/index.html"
+  }
+];
